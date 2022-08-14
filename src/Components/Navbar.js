636b78c4c7d2f1f3c477d339f3f2ef781cf71790
ashrafur22 <ashrafurrahman22@gmail.com>
2022-08-14 10:26:51 +0600

    updated-project

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,48 +1,82 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import navlogo from '../assetts/hetali logo_nav.png'
+import '../Styles/Nav.css'
 
 const Navbar = () => {
     return (
-        <div class="navbar bg-base-100">
+        <div class="navbar bg-base-200 px-10">
   <div class="navbar-start">
     <div class="dropdown">
       <label tabindex="0" class="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li tabindex="0">
-          <a class="justify-between">
-            Parent
-            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-          </a>
-          <ul class="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+      <li><Link to='/' className='flex flex-col place-items-start'>
+        About Us
+        <div id='hello'></div>
+        </Link></li>
+      <li><Link to='/projects' className='flex flex-col place-items-start'>
+        Projects
+        <div id='hello'></div>
+        </Link></li>
+      <li><Link to='/diversification' className='flex flex-col place-items-start'>
+        Diversification
+        <div id='hello'></div>
+        </Link></li>
+      <li><Link to='/ourProcess' className='flex flex-col place-items-start'>
+        Our Process
+        <div id='hello'></div>
+        </Link></li>
+      <li><Link to='/testimonials' className='flex flex-col place-items-start'>
+        Testimonials
+        <div id='hello'></div>
+        </Link></li>
+      <li><Link to='/careers' className='flex flex-col place-items-start'>
+        Careers
+        <div id='hello'></div>
+        </Link></li>
+      <li><Link to='/contact' className='flex flex-col place-items-start'>
+        Contact Us
+        <div id='hello'></div>
+        </Link></li>
       </ul>
     </div>
-    <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <Link to='/' class="w-36">
+      <img src={navlogo} alt="" />
+    </Link>
   </div>
   <div class="navbar-center hidden lg:flex">
-    <ul class="menu menu-horizontal p-0">
-      <li><a>Item 1</a></li>
-      <li tabindex="0">
-        <a>
-          Parent
-          <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-        </a>
-        <ul class="p-2">
-          <li><a>Submenu 1</a></li>
-          <li><a>Submenu 2</a></li>
-        </ul>
-      </li>
-      <li><a>Item 3</a></li>
+    <ul class="menu menu-horizontal p-0 uppercase">
+      <li><Link to='/' className='flex flex-col place-items-start'>
+        About Us
+        <div id='hello'></div>
+        </Link></li>
+      <li><Link to='/projects' className='flex flex-col place-items-start'>
+        Projects
+        <div id='hello'></div>
+        </Link></li>
+      <li><Link to='/diversification' className='flex flex-col place-items-start'>
+        Diversification
+        <div id='hello'></div>
+        </Link></li>
+      <li><Link to='/ourProcess' className='flex flex-col place-items-start'>
+        Our Process
+        <div id='hello'></div>
+        </Link></li>
+      <li><Link to='/testimonials' className='flex flex-col place-items-start'>
+        Testimonials
+        <div id='hello'></div>
+        </Link></li>
+      <li><Link to='/careers' className='flex flex-col place-items-start'>
+        Careers
+        <div id='hello'></div>
+        </Link></li>
+      <li><Link to='/contact' className='flex flex-col place-items-start'>
+        Contact Us
+        <div id='hello'></div>
+        </Link></li>
     </ul>
-  </div>
-  <div class="navbar-end">
-    <a class="btn">Get started</a>
   </div>
 </div>
     );
